5893e5cca8f1997fa31f2617b7c9000717cbe8db
Marcio Oliveira <root@ifropvo01vmas03.ifro.local>
2017-12-29 08:53:49 -0400

    Add ntp.pp and params.pp

---
+++ ntp/manifests/ntp.pp
@@ -0,0 +1,16 @@
+class ifro_ntp::ntp {
+	include ifro_ntp::params
+exec { "apt-update":
+	command => "/usr/bin/apt-get update"
+}
+package	{ 'ntp':
+	ensure	=>	installed,
+	require => Exec["apt-update"],
+}
+
+service	{	'ntp':
+	ensure => running,
+	enable => true,
+	require	=>	[Package['ntp']]
+}
+}
